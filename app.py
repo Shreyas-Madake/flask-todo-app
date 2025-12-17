a3d0aa56c109e23d4d2da9223e0de3174ac12170
SHREYAS MADAKE <madakeshreyas@gmail.com>
2025-12-17 15:42:40 +0530

    Update app.py for Render port compatibility

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -61,4 +62,7 @@ def delete(sno):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))
+    
+    app.run(host='0.0.0.0', port=port)
